55f06fdd4bb7a5cda8d1cd18119dfe74aa33fd1b
imamnurhy <imamnurhy@gmail.com>
2021-12-08 13:37:14 +0700

    FIx : convert pdf to image

---
+++ lib/pdf_to_image.dart
@@ -0,0 +1,62 @@
+library signature;
+
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf_render/pdf_render.dart';
+import 'package:image/image.dart' as imglib;
+
+class PdfToImage {
+  Future<Map<String, dynamic>> convert(String url, int pageNumber) async {
+    final ByteData bytes = await NetworkAssetBundle(Uri.parse(url)).load(url);
+    final Uint8List list = bytes.buffer.asUint8List();
+    List<imglib.Image> _images = [];
+    final doc = await PdfDocument.openData(list);
+
+    var page = await doc.getPage(pageNumber);
+    var totalPage = doc.pageCount;
+    var imgPDF = await page.render();
+    var img = await imgPDF.createImageDetached();
+    dynamic imgBytes = await img.toByteData(format: ImageByteFormat.png);
+    dynamic libImage = imglib.decodeImage(
+      imgBytes.buffer.asUint8List(
+        imgBytes.offsetInBytes,
+        imgBytes.lengthInBytes,
+      ),
+    );
+
+    _images.add(libImage);
+
+    var height = libImage.height;
+    var width = libImage.width;
+
+    final mergedImage = imglib.Image(width, height);
+    for (var element in _images) {
+      imglib.copyInto(
+        mergedImage,
+        element,
+        dstX: 0,
+        dstY: 0,
+        blend: false,
+      );
+    }
+
+    String nameWithExtentions = url.split('/').last;
+    String nameWithoutExtentions = nameWithExtentions.split('.').first;
+    final String fileName = nameWithoutExtentions + '$pageNumber' + '.jpg';
+
+    final documentDirectory = await getExternalStorageDirectory();
+    File imgFile = File('${documentDirectory!.path}/' + fileName);
+    File(imgFile.path).writeAsBytesSync(imglib.encodeJpg(mergedImage));
+
+    return {
+      'height': height.toDouble(),
+      'width': width.toDouble(),
+      'image': imgFile,
+      'total_page': totalPage,
+    };
+  }
+}
---
+++ lib/signature.dart
@@ -0,0 +1,221 @@
+library signature;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'pdf_to_image.dart';
+
+class Signature extends StatefulWidget {
+  final double boxHeight, boxWidth;
+
+  final StreamController<Map> stream;
+
+  final Widget child;
+
+  final String fileUrl;
+
+  const Signature({
+    Key? key,
+    this.boxHeight = 40,
+    this.boxWidth = 160,
+    required this.stream,
+    required this.child,
+    required this.fileUrl,
+  }) : super(key: key);
+
+  @override
+  _SignatureState createState() => _SignatureState();
+}
+
+class _SignatureState extends State<Signature> {
+  // Controller
+  final TransformationController _transformationController = TransformationController();
+
+  // Utilities
+  final PdfToImage _convertPdfToImage = PdfToImage();
+
+  @override
+  void initState() {
+    _transformationController.value = Matrix4.identity() * 0.01;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _transformationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Map>(
+      future: _convertPdfToImage.convert(widget.fileUrl, 1),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return SizedBox(
+            height: snapshot.data!['height'],
+            width: snapshot.data!['width'],
+            child: InteractiveViewer(
+              minScale: 0.1,
+              maxScale: 5.0,
+              constrained: false,
+              transformationController: _transformationController,
+              child: Stack(
+                children: [
+                  // PDF
+                  Image.file(
+                    snapshot.data!['image'],
+                    fit: BoxFit.cover,
+                  ),
+
+                  // Signature
+                  _SignatureBox(
+                    canvasHeight: snapshot.data!['height'],
+                    canvasWidth: snapshot.data!['width'],
+                    boxHeight: widget.boxHeight,
+                    boxWidth: widget.boxWidth,
+                    child: widget.child,
+                    boxStream: widget.stream,
+                    pageNumber: 1,
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text('No Document'),
+          );
+        }
+      },
+    );
+  }
+}
+
+/// Signature Box
+/// ----
+/// Kotak gambar tanda tangan yang bisa di geser dengan cara menekan lama si box tersebut
+///
+class _SignatureBox extends StatefulWidget {
+  final double canvasHeight;
+  final double canvasWidth;
+  final double boxHeight, boxWidth;
+
+  final int pageNumber;
+
+  final StreamController<Map> boxStream;
+
+  final Widget child;
+
+  const _SignatureBox({
+    Key? key,
+    required this.canvasHeight,
+    required this.canvasWidth,
+    this.boxHeight = 160,
+    this.boxWidth = 40,
+    required this.boxStream,
+    required this.child,
+    required this.pageNumber,
+  }) : super(key: key);
+
+  @override
+  __SignatureBoxState createState() => __SignatureBoxState();
+}
+
+class __SignatureBoxState extends State<_SignatureBox> {
+  double initX = 0.0, initY = 0.0;
+  Offset _offset = Offset.zero;
+  double _updateScale = 1.0, _initialScale = 1.0;
+
+  @override
+  void initState() {
+    // Initial location box in canvas
+    initX = (widget.canvasWidth - 200) / 2;
+    initY = (widget.canvasHeight - 200) / 2;
+    _offset = Offset(initX, initY);
+
+    // Initial Box Stream
+    widget.boxStream.sink.add({
+      'page': widget.pageNumber,
+      'posx': initX,
+      'posy': initY,
+      'height': widget.boxHeight,
+      'width': widget.boxWidth,
+      'llx': initX,
+      'lly': initY + widget.boxHeight,
+      'urx': initX + widget.boxWidth,
+      'ury': initY,
+      'llx_trans': initX,
+      'lly_trans': widget.canvasHeight - (initY + widget.boxHeight),
+      'urx_trans': initX + widget.boxWidth,
+      'ury_trans': widget.canvasHeight - initY,
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: _offset,
+      child: Transform.scale(
+        scale: _updateScale,
+        child: GestureDetector(
+          onLongPressStart: (details) {
+            setState(() {
+              initX = details.globalPosition.dx;
+              initY = details.globalPosition.dy;
+            });
+          },
+          onLongPressMoveUpdate: (details) {
+            double dx, dy;
+            setState(() {
+              dx = details.globalPosition.dx - initX;
+              dy = details.globalPosition.dy - initY;
+              initX = details.globalPosition.dx;
+              initY = details.globalPosition.dy;
+              _offset = _offset + Offset(dx, dy);
+            });
+
+            widget.boxStream.sink.add({
+              'page': widget.pageNumber,
+              'posx': _offset.dx,
+              'posy': _offset.dy,
+              'height': widget.boxHeight,
+              'width': widget.boxWidth,
+              'llx': _offset.dx,
+              'lly': _offset.dy + widget.boxHeight,
+              'urx': _offset.dx + widget.boxWidth,
+              'ury': _offset.dy,
+              "llx_trans": (_offset.dx).toInt(),
+              "lly_trans": (widget.canvasHeight - (_offset.dy + widget.boxHeight)).toInt(),
+              "urx_trans": (_offset.dx + widget.boxWidth).toInt(),
+              "ury_trans": (widget.canvasHeight - _offset.dy).toInt(),
+            });
+          },
+          onScaleStart: (details) {
+            _initialScale = _updateScale;
+          },
+          onScaleUpdate: (details) {
+            setState(() {
+              _updateScale = _initialScale * details.scale;
+            });
+          },
+          child: Container(
+            height: widget.boxHeight,
+            width: widget.boxWidth,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+              ),
+            ),
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
